fc1bd9f0b9ae2bdc10ba32a031c13964483cfeba
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-06 21:07:14 +0300

    Add register logic

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 
 import User from '../models/User.js';
 
@@ -7,7 +8,18 @@ const router = express.Router();
 router.post('/register', async(req, res) => { 
     try {
         const {email, password} = req.body;
+        const candidate = await User.findOne({ email })
 
+        if (candidate){
+            return res.status(400).json({ message: "We've already welcomed user with this email..."})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = new User({ email, password: hashedPassword });
+
+        await user.save();
+
+        res.status(201).json({ message: "User has been successfully created"});
 
     } catch (error) {
         res.status(500).json({ message: "Oops...Server error" });
