db7e55cec39109c2c042c7e9452942684fb3bba3
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-06 13:13:35 +0300

    Add port

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
-console.log("I love you!")
+import express from 'express';
+import config from 'config';
+import mongoose from 'mongoose';
+
+const app = express();
+
+const PORT = config.get("PORT") || 5000;
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`App is alive on the port ${PORT}!`);
+})
