5d8030ffd1b18fc0cd9b86ebdadd8f3eeb816d47
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-06 20:18:42 +0300

    Add mongoDB connection

--- App.js
+++ App.js
@@ -1,1 +1,25 @@
-console.log("You can do it!")
+import express from 'express';
+import config from 'config';
+import mongoose from 'mongoose';
+
+const app = express();
+
+const PORT = config.get('port') || 5000;
+
+async function startDB() {
+    try{
+        await mongoose.connect(config.get('mongoDBUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(5000, () => console.log(`App is alive at post ${PORT}`))
+    } catch (error){
+        console.log('Server Error: ', error.message);
+        process.exit(1)
+    }
+};
+
+startDB();
+
+
