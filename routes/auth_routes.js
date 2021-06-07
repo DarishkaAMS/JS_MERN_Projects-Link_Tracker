ed71c17393e397d41d8ddbe4c596369136880f3e
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-07 05:55:35 +0300

    Add user login logic and validation

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,6 +1,8 @@
-import express from 'express';
 import bcrypt from 'bcryptjs';
+import config from 'config';
+import express from 'express';
 import { check, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import User from '../models/User.js';
 
@@ -41,11 +43,45 @@ router.post(
     };
 });
 
-router.post('/login', async(req, res) => { 
+router.post(
+    '/login', 
+    [
+        check('email', "Please give me a correct email").normalizeEmail().isEmail(),
+        check('password', "What is your password?").exists()
+    ],
+    async(req, res) => { 
     try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Sorry, but I can't login you with this data"
+            })
+        };
+
+    const {email, password} = req.body; 
+    const user = await User.findOne({ email });
+
+    if (!user){
+        return res.status(400).json({ message: "Hmm... There is no such user" })
+    };
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch){
+        return res.status(400).json({ message: "Hmm... Last time you use different password" })
+    };
+
+    const token = jwt.sign(
+        { userId: user.id },
+        config.get("jwtSecretKey"),
+        { expiresIn: '1h'}
+    );
+
+    res.status(200).json({ token, userId: user.id })
 
     } catch (error) {
-
+        res.status(500).json({ message: "Oops...Server error" });
     };
 });
 
