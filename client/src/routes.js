c9ffb4b0286633fb1ce90a8d161f4afa00fe1e5f
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-07 06:49:38 +0300

    Setup routes

---
+++ client/src/routes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import { AuthPage } from './bages/auth_page.js';
+import { CreatePage } from './bages/create_page.js';
+import { DetailPage } from './bages/detail_page.js';
+import { LinksPage } from './bages/links_page.js';
+
+export const useRoutes = isAuthenticated => {
+    if(isAuthenticated){
+        return (
+            <Switch>
+                <Route path="/create" exact>
+                    <LinksPage />
+                </Route>
+                <Route path="/links" exact>
+                    <CreatePage />
+                </Route>
+                <Route path="/detail/:id">
+                    <DetailPage />
+                </Route>
+                <Redirect to="/create" />
+            </Switch>
+        )
+    };
+    return (
+        <Switch>
+            <Route path="/" exact>
+                <AuthPage />
+            </Route>
+            <Redirect to="/" />
+        </Switch>
+    )
+};
